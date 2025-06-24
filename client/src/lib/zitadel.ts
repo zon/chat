ffe3f542a6099c23bcba7d81e55d34a303d65227
Matthew Haralovich <zon@haralovich.org>
2025-06-23 22:13:20 -0400

    Tried to puth auth in local storage

--- client/src/lib/zitadel.ts
+++ client/src/lib/zitadel.ts
@@ -1,7 +1,8 @@
 import { createZITADELAuth } from '@zitadel/vue'
-import { type OidcAuth } from 'vue-oidc-client/vue3'
+import { SignInType, type OidcAuth } from 'vue-oidc-client/vue3'
 import { fatalError } from './error'
 import { clearAuth, updateAuth } from './auth'
+import { WebStorageStateStore } from 'oidc-client'
 
 export interface ZitadelAuth {
   oidcAuth: OidcAuth
@@ -11,10 +12,14 @@ export interface ZitadelAuth {
 export let zitadelAuth: ZitadelAuth
 
 export async function initZitadel() {
+
   zitadelAuth = createZITADELAuth({
     project_resource_id: import.meta.env.VITE_ZITADEL_PROJECT_RESOURCE_ID,
     client_id: import.meta.env.VITE_ZITADEL_CLIENT_ID,
     issuer: import.meta.env.VITE_ZITADEL_ISSUER,
+  }, undefined, SignInType.Window, undefined, {
+    automaticSilentRenew: true,
+    stateStore: new WebStorageStateStore({ store: localStorage }),
   })
 
   zitadelAuth.oidcAuth.events.addSilentRenewError(fatalError)
