16f6c4ecd418cf5007b16734894b642d42140583
Matthew Z Haralovich <Zon zon@haralovich.org>
2025-07-03 12:58:01 -0400

    New users once again directed to auth screen

--- client/src/components/views/Authorizing.vue
+++ client/src/components/views/Authorizing.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { auth } from '@/lib/auth';
+import { auth, authUser } from '@/lib/auth';
 import { onMounted } from 'vue'
 import { useRouter } from 'vue-router';
 import Status from '../Status.vue';
@@ -9,7 +9,11 @@ const router = useRouter()
 onMounted(async () => {
   const done = await auth()
   if (done) {
-    router.replace('/')
+    if (authUser.value.ready) {
+      router.replace('/')
+    } else {
+      router.replace('/auth')
+    }
   }
 })
 
